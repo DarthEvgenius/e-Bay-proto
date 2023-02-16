ee139dbf01f8bf554130f4520f73ad2431a0f128
“Eugene_Zinin” <EugeneZinin@gmail.com>
2023-02-16 20:52:48 +0300

    Added models: Listings, Categories

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# admin usernamme = admin, pass = 0000
+
+from .models import User, Categories, Listings
 
 # Register your models here.
+
+# View style in admin-app
+class UserAdmin(admin.ModelAdmin):
+	list_display = ("id", "username", "first_name", "last_name", "email")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Categories)
+admin.site.register(Listings)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Categories(models.Model):
+    category = models.CharField(max_length=20)
+
+class Listings(models.Model):
+    title = models.CharField(max_length=50)
+    # Who is owner of the listing
+    # We can look for this user's other listings
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_listings")
+    init_price = models.IntegerField()
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="category_listings")
+    # If the listing is active or not
+    status = models.BooleanField()
+    image = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+
